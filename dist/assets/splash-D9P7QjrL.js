28f49a983f71520c059bcc9cdc163b9dd0aef349
shashank <shashankraj007281@gmail.com>
2025-06-29 23:27:20 +0530

    fixed -resource not load- error on cloud builds

---
+++ dist/assets/splash-D9P7QjrL.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";var e;(e=window.electronAPI)!=null&&e.getAppVersion&&window.electronAPI.getAppVersion().then(n=>{document.getElementById("version").innerText=`v${n}`});
